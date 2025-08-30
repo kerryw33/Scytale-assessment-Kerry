f2062600b8aaf672416deeadf1e33c58cb250839
Kerry-Lynn Whyte <kerry-lynnwhyte@Kerry-Lynns-MacBook-Pro.local>
2025-08-30 13:04:48 +0200

    Fix token handling and update requirements

---
+++ data/processed/PR_audit.cs
@@ -0,0 +1,5 @@
+number,title,author,base,head,merge_commit,merged_at,CR_PASSED,CHECKS_PASSED
+1,Chore/ops 197/last fixes before publishing,aviyashar,master,chore/OPS-197/last-fixes-before-publishing,4b43403e952097d8d96e781b5b8239f5da9378c7,2023-11-21T12:23:48Z,False,False
+2,Update README.md,aviyashar,feature/eva,aviyashar-patch-1,157ef294248d773a3abeed5b8e6ff654c161358d,2023-11-21T12:25:14Z,False,False
+3,Feature/eva,aviyashar,master,feature/eva,f77d78620d580ad4787630c0daef3121ebad9ddb,2023-11-21T12:27:11Z,False,False
+4,avi pull request,aviyashar,master,aviyashar-patch-1,67922c374fda7007e0fd5c83a3d6fba20fe4f125,2023-11-21T12:29:07Z,False,False
